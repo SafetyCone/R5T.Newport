16264070adb30f0bf6b40acea1eaced1fb2b711b
David Coats <david@getsafetycone.com>
2021-05-17 22:11:46 -0700

    Add R5T.Newport.Extensions library.

---
+++ source/R5T.Newport.Extensions/Code/Helpers/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+using R5T.Magyar.IO;
+
+
+namespace Newtonsoft.Json
+{
+    public static class JsonHelper
+    {
+        public static async Task<JObject> LoadAsJObject(string jsonFilePath)
+        {
+            using (var streamReader = StreamReaderHelper.New(jsonFilePath))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
+            {
+                var output = await JObject.LoadAsync(jsonTextReader);
+                return output;
+            }
+        }
+    }
+}
